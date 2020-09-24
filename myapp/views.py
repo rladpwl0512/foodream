130255f3713347f955c63bc76d0272b6f5783275
yeji <rladpwl0512@naver.com>
2020-09-24 21:45:05 +0900

    home

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from upload.models import Form
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    forms = Form.objects
+    count = Form.objects.count()
+    return render(request, 'home.html', {'context' : count, 'forms': forms})
+
+# def detail(request, form_id):
+#     form_detail =get_object_or_404(Form, pk = form_id)
+#     return render(request, 'detail.html', {'form': form_detail})
